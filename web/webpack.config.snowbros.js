73c3c05622d6ac657ab9bc5f100843ac5e67f607
Noah Cohn <noahc42@gmail.com>
2026-03-20 16:00:38 -0700

    Create webpack.config.snowbros.js

---
+++ web/webpack.config.snowbros.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.REACT_APP_MAME_JS_PATH': JSON.stringify(process.env.REACT_APP_MAME_JS_PATH || 'snowbros.js'),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: process.env.PORT || 8080,
+    hot: true,
+  },
+};
